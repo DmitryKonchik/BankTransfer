2cac98a07555c44910fd9326b3a95d3c40d04b9e
Dmitry <dkonchik@gmail.com>
2024-06-11 21:55:13 +0300

    comments translated 1.1

--- src/servises/CountTransfer.java
+++ src/servises/CountTransfer.java
@@ -25,14 +25,14 @@ public class CountTransfer {
     private Map<String, Double> mapOfCounts = parseInfoAboutCount.getCounts();
 
     public void parseTransactionFiles() {
-        // the method looks to see if there are files in the transactions folder suitable for conducting transactions and processes all files through a loop
+        // The method looks to see if there are files in the transactions folder suitable for conducting transactions and processes all files through a loop
         File inputFolder = new File(PATH_TO_INPUT_FOLDER);
         File[] files = inputFolder.listFiles();
-        if (files != null) { // check if there are suitable files
+        if (files != null) { // Check if there are suitable files
             for (File file : files) {
                 if (file.isFile() && file.getName().endsWith(".txt")) {
                     readAndTransaction(file);
-                    moveFileToArchive(file); // move the transaction file to the archive
+                    moveFileToArchive(file); // Move the transaction file to the archive
                 } else {
                     moveFileToArchive(file);
                 }
@@ -56,28 +56,28 @@ public class CountTransfer {
     }
 
     public void transaction(String line, File file) {
-        // method for transaction
+        // Method for transaction
 
         BankCount countToTransaction; // The account to which we transfer
         BankCount countFromTransaction; // Account from which we transfer
-        String resultOfTransaction; // transaction result
+        String resultOfTransaction; // Transaction result
 
         Pattern pattern = Pattern.compile(PATTERN_TO_CHECK_TRANSACTION_LINE);
         Matcher matcher = pattern.matcher(line);
 
-        if (matcher.find()) { // checking the correctness of the entered string in the transaction file
+        if (matcher.find()) { // Checking the correctness of the entered string in the transaction file
             line = matcher.group();
             if (isCountExist(line.substring(5, 16)) && isCountExist(line.substring(20, 31))) {
                 // Checking whether such accounts exist
-                countFromTransaction = findCount(line.substring(5, 16));// create an account from which money is transferred
-                countToTransaction = findCount(line.substring(20, 31));// create an account to which money is transferred
-                double money = Double.parseDouble(line.substring(34)); // from the transaction file we assign the value of the transferred money to a variable
-                if (money > 0.0) { // checking whether the amount to be transferred is positive
-                    if (countFromTransaction.isEnoughMoneyOnBalance(money)) {// we check whether there is enough money in the account from which we are transferring money
-                        countToTransaction.addMoney(money); // adding money to your account
-                        countFromTransaction.withdrawMoney(money); // take money from the account
+                countFromTransaction = findCount(line.substring(5, 16));// Create an account from which money is transferred
+                countToTransaction = findCount(line.substring(20, 31));// Create an account to which money is transferred
+                double money = Double.parseDouble(line.substring(34)); // From the transaction file we assign the value of the transferred money to a variable
+                if (money > 0.0) { // Checking whether the amount to be transferred is positive
+                    if (countFromTransaction.isEnoughMoneyOnBalance(money)) {// Check whether there is enough money in the account from which we are transferring money
+                        countToTransaction.addMoney(money); // Adding money to your account
+                        countFromTransaction.withdrawMoney(money); // Take money from the account
                         resultOfTransaction = "Successful transaction";
-                        // write data to MAP after the transaction
+                        // Write data to MAP after the transaction
                         parseCountToMap(countFromTransaction);
                         parseCountToMap(countToTransaction);
                     } else {
@@ -92,7 +92,7 @@ public class CountTransfer {
         } else {
             resultOfTransaction = "Invalid transaction string";
         }
-        // writing a report to a file after a transaction
+        // Writing a report to a file after a transaction
         parseReport(file, resultOfTransaction, line);
     }
 
@@ -118,7 +118,7 @@ public class CountTransfer {
     }
 
     public void moveFileToArchive(File file) {
-        // method for moving a file to an archive
+        // Method for moving a file to an archive
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss.SSS");
         try {
             Files.move(Paths.get(file.getPath()), Paths.get(PATH_TO_ARCHIVE_FOLDER + LocalDateTime.now().format(formatter) + file.getName()));
